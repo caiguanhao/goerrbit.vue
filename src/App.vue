8aff2ea28159668b909957a41ccbc85fd9c10df9
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2021-06-01 11:28:41 +0800

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <template>
-  Hello
+  <header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Errbit</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Apps</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <main class="flex-shrink-0">
+    <div class="container">
+      Hello
+    </div>
+  </main>
+
+  <footer class="footer mt-auto py-3 bg-light">
+    <div class="container">
+      <span class="text-muted"><a href="https://github.com/caiguanhao/goerrbit" target="_blank">goerrbit</a></span>
+    </div>
+  </footer>
 </template>
 
 <script>
 </script>
 
 <style>
+main > .container {
+  padding-top: 80px;
+}
 </style>
